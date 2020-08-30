be254cd3340d869f05bb6ce8986bbaecadb89343
dujianjun <v@jianwi.cn>
2020-08-31 00:05:58 +0800

    集合间操作

--- 集合/集合.js
+++ 集合/集合.js
@@ -51,26 +51,105 @@ function Set()
     {
         return Object.values(this.item)
     }
+    // 集合间的操作
+    // 并集
+    Set.prototype.union = function(otherSet)
+    {
+        // 创建新集合
+        let unionSet = new Set()
+        // 将 this.item 添加到 unionSet
+        for(let item of this.values()){
+            unionSet.add(item)
+        }
+        // 将 other 有，this.item 没有的部分添加到 unionSet
+        for(let item of otherSet.values()){
+            if (this.values().includes(item)){
+                continue
+            }
+            unionSet.add(item)
+        }
+        return unionSet
+    }
+    // 差集
+    Set.prototype.difference = function(otherSet)
+    {
+        let diff_set = new Set()
+        // this.item 有，otherSet 没有的
+        for(let item of this.values()){
+            if(otherSet.values().includes(item)){
+                continue
+            }
+            diff_set.add(item)
+        }
+        // otherSet 有， this.item 没有的
+        for(let item of otherSet.values())
+        {
+            if(this.values().includes(item)){
+                continue
+            }
+            diff_set.add(item)
+        }
+        return diff_set
+    }
+    // 子集
+    Set.prototype.subset = function(otherSet)
+    {
+        for (let item of otherSet.values()){
+            if(!this.has(item)){
+                return false
+            }
+        }
+        return true
+    }
 
 }
 
 // test
 
-let set = new Set()
-set.add(11333)
-set.add("第二个")
-set.add("777")
-set.add(123)
-console.log(set.has("123"))
-// 可以区别类型
-console.log(set.has(123))
+// let set = new Set()
+// set.add(11333)
+// set.add("第二个")
+// set.add("777")
+// set.add(123)
+// console.log(set.has("123"))
+// // 可以区别类型
+// console.log(set.has(123))
 
 
-console.log(set+"")
+// console.log(set+"")
 
-set.remove(123)
-console.log(set.remove("777"))
-console.log(set.values())
-set.clear()
-console.log(set+"")
-console.log(set.values())
+// set.remove(123)
+// console.log(set.remove("777"))
+// console.log(set.values())
+// set.clear()
+// console.log(set+"")
+// console.log(set.values())
+
+// 测试集合间操作
+
+// 交集
+
+let a = new Set()
+a.add("a")
+a.add("b")
+a.add("c")
+
+let b = new Set()
+b.add("a")
+b.add("c")
+b.add("e")
+b.add("f")
+
+console.log(a.union(b).values())
+
+// 差集
+console.log(a.difference(b).values())
+
+// 子集
+let c = new Set()
+c.add("a")
+c.add("b")
+
+console.log(a.subset(c))
+
+console.log(a.subset(b) )
