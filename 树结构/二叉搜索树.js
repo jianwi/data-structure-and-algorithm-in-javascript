51d8cb55ae95ae8ba47dbddceaf26963b6072888
dujianjun <v@jianwi.cn>
2020-09-07 16:08:07 +0800

    封装u二叉树

---
+++ 树结构/二叉搜索树.js
@@ -0,0 +1,14 @@
+function BinarySearchTree() {
+
+    function node(key) {
+        this.key = key
+        this.left = null
+        this.right = null
+    }
+
+    //属性
+    this.root = null
+//    方法
+
+
+}
